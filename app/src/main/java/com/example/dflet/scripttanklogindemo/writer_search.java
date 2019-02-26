b4a42cbc8acce56f529bb439b9ade2622681f211
Zach W <zweebees@gmail.com>
2019-02-25 20:33:23 -0500

    Created button for a navigation view
    Created a button that will open a side panel for navigation (only in search UI for now, but will add to other layouts once I know it works)

--- app/src/main/java/com/example/dflet/scripttanklogindemo/writer_search.java
+++ app/src/main/java/com/example/dflet/scripttanklogindemo/writer_search.java
@@ -1,13 +1,39 @@
 package com.example.dflet.scripttanklogindemo;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class writer_search extends AppCompatActivity {
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writer_search);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch(item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
