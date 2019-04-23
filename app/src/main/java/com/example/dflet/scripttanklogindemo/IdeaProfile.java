f592bbc06d97a5f82587ac1cdd1f803e4020777a
Zach W <zweebees@gmail.com>
2019-04-23 00:41:27 -0400

    Prototype for Idea 'profile'
    Created UI for the the screen that pops up when you click on a search result. This will probably change a lot from this original concept

---
+++ app/src/main/java/com/example/dflet/scripttanklogindemo/IdeaProfile.java
@@ -0,0 +1,13 @@
+package com.example.dflet.scripttanklogindemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IdeaProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idea_profile);
+    }
+}
