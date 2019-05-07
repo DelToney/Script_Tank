6b9995fa2c53455eff857f2f6047f2eec3eb3df3
Zach W <zweebees@gmail.com>
2019-05-07 17:39:53 -0400

    Added some more stuff to publisher idea list
    Added more of the code for the list (need to test it). Also created a fun image for the welcome screen.

--- app/src/main/java/com/example/dflet/scripttanklogindemo/PublisherIdeaList.java
+++ app/src/main/java/com/example/dflet/scripttanklogindemo/PublisherIdeaList.java
@@ -1,10 +1,12 @@
 package com.example.dflet.scripttanklogindemo;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,12 +22,13 @@ import java.util.Map;
 public class PublisherIdeaList extends AppCompatActivity {
 
     private static User m_User;
+    private Toolbar toolbar;
     protected ScriptTankApplication myApp;
     private RecyclerView recyclerView;
     private ArrayList<String> ideaTitles;
     private ArrayList<String> writers;
-    //MAKE ADAPTER JAVA FILE
-    //ARRAY LIST OF OBJECTS
+    private SearchResultAdapter adapter;
+    private ArrayList<TestItem> testItems;
     private FirebaseFunctions functions;
 
     @Override
@@ -35,7 +38,40 @@ public class PublisherIdeaList extends AppCompatActivity {
         myApp = (ScriptTankApplication)this.getApplicationContext(); //these three lines need to be in every
         m_User = myApp.getM_User();
         functions = FirebaseFunctions.getInstance();
-        getPublisherIdeas(m_User.fb_id);
+        toolbar = findViewById(R.id.toolbar);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        setSupportActionBar(toolbar);
+
+        getPublisherIdeas(m_User.getKey()).addOnCompleteListener(new OnCompleteListener<HashMap<String, Object>>(){
+            public void onComplete(@NonNull Task<HashMap<String, Object>> task){
+                // If the firebase function executes successfully
+                if(task.isSuccessful()){
+                    // Log 'success' in the Logcat
+                    Log.d("Success", "Success");
+                    // Set results to the result of the firebase function
+                    HashMap<String, Object> results = task.getResult();
+                    // Update RecyclerView with results
+                    updateListAdapter(results);
+                    // Debug stuff
+                    System.out.println(results.size());
+                    System.out.println(results);
+                }
+                else {
+                    // If the firebase fails
+                    Exception e = task.getException();
+                    if (e instanceof FirebaseFunctionsException) {
+                        // Logs error in the firebase console
+                        FirebaseFunctionsException ffe = (FirebaseFunctionsException) e;
+                        FirebaseFunctionsException.Code code = ffe.getCode();
+                        Object details = ffe.getDetails();
+                    }
+                    // Prints error message in Logcat
+                    System.err.println(e.getMessage());
+                }
+            }
+
+        });
     }
 
 
@@ -55,6 +91,22 @@ public class PublisherIdeaList extends AppCompatActivity {
                         return result;
                     }
                 });
+    }
+
+    //Update List Adapter
+    private void updateListAdapter(HashMap<String, Object> results){
+        // Arrays for different text in the RecyclerView entry
+        ideaTitles = (ArrayList<String>)results.get("Ideas");
+        writers = (ArrayList<String>)results.get("Writers");
+
+        // For each entry in ideaTitles
+        for(int i = 0; i < ideaTitles.size(); i++) {
+            // Add new search result
+            testItems.add(new TestItem(R.drawable.ic_person_black_24dp, ideaTitles.get(i), writers.get(i)));
+        }
+
+        // Notify the adapter that the data has changed, changing the RecyclerView entries
+        adapter.notifyDataSetChanged();
     }
 
 
