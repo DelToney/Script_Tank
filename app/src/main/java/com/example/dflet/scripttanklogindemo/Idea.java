ce9e2e406a6062edb9784da13669c691e0b352d7
Zach W <zweebees@gmail.com>
2019-05-08 02:02:01 -0400

    Small Fixes
    Forgot to stage the files

--- app/src/main/java/com/example/dflet/scripttanklogindemo/Idea.java
+++ app/src/main/java/com/example/dflet/scripttanklogindemo/Idea.java
@@ -1,4 +1,31 @@
 package com.example.dflet.scripttanklogindemo;
 
-public class Idea {
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.io.Serializable;
+
+@IgnoreExtraProperties
+public class Idea implements Serializable {
+
+    public String IdeaName, Abstract, Description, Genre, Publisher, Length,  key, WriterID, WriterName, Worth, WriterPicture;
+
+    public Idea() {
+        return;
+    }
+
+    public Idea(String IdeaName, String ideaAbstract, String ideaDescription, String genre, String length, String authorName, String authorID, String ideaWorth) {
+        this.IdeaName = IdeaName;
+        this.Abstract = ideaAbstract;
+        this.Description = ideaDescription;
+        this.Genre = genre;
+        this.Length = length;
+        this.Publisher = null;
+        this.WriterID = authorID;
+        this.WriterName = authorName;
+        this.Worth = ideaWorth;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
 }
--- app/src/main/java/com/example/dflet/scripttanklogindemo/WriterProfileActivity.java
+++ app/src/main/java/com/example/dflet/scripttanklogindemo/WriterProfileActivity.java
@@ -1,13 +1,89 @@
 package com.example.dflet.scripttanklogindemo;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.HttpsCallableResult;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class WriterProfileActivity extends AppCompatActivity {
+
+    protected ScriptTankApplication myApp;
+    private static User m_User, profiledUser;
+    TextView mWriterName, mDetails, mBio;
+    String mEmail, mPhoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writer_profile);
+        myApp = (ScriptTankApplication)this.getApplicationContext();
+        m_User = myApp.getM_User();
+        myApp.setCurrActivity(this);
+
+        profiledUser = ((m_User == myApp.getmCurrentUser())?m_User:myApp.getmCurrentUser());//when adding parcellable user id, itll go into null
+
+
+        mWriterName = findViewById(R.id.authorTitle);
+        mBio = findViewById(R.id.AuthorBio);
+        mDetails = findViewById(R.id.authorDetails);
+
+
+        GetUserInfo(profiledUser.key).addOnCompleteListener(new OnCompleteListener<HashMap<String, Object>>() {
+                                                                @Override
+                                                                public void onComplete(@NonNull Task<HashMap<String, Object>> task) {
+                                                                    System.out.println(task.getResult());
+                                                                    UpdateProfileInfo(task.getResult());
+                                                                }
+                                                            }
+        );
+
+        final Button viewIdeasButton = findViewById(R.id.viewIdeas);
+        viewIdeasButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myApp.setmCurrentUser(profiledUser);
+                Intent intent = new Intent(WriterProfileActivity.this,
+                        ViewUploadsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void UpdateProfileInfo(HashMap<String, Object> results) {
+        mWriterName.setText((String)results.get("name"));
+        mEmail = (String)results.get("email");
+        mPhoneNumber = (String)results.get("phoneNumber");
+        mDetails.setText(mEmail + "\n" + mPhoneNumber);
+    }
+
+    private Task<HashMap<String, Object>> GetUserInfo(String key) {
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("key", key);
+
+        FirebaseFunctions ff = FirebaseFunctions.getInstance();
+
+        return ff
+                .getHttpsCallable("loadUserProfileByKey")
+                .call(data)
+                .continueWith(new Continuation<HttpsCallableResult, HashMap<String, Object>>() {
+                    @Override
+                    public HashMap<String, Object> then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        HashMap<String, Object> result = (HashMap<String, Object>) task.getResult().getData();
+                        return result;
+                    }
+                });
     }
 }
